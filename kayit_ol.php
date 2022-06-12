a14165a1eb91d857df33b7c7910221d4a31a027f
ozturk sirin <ozturk2002.zri@gmail.com>
2022-06-12 21:20:16 +0300

    clear

--- kayit_ol.php
+++ kayit_ol.php
@@ -13,12 +13,35 @@ include("baglan.php");
 
     <link rel="stylesheet" type="text/css" href="style.css"> 
 
-    <h1>KAYIT OL</h1>
-
     
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <header>
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="./index.php">ANASAYFA</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="./hakkında.php">HAKKINDA <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="./iletisim.php">İLETİŞİM</a>
+      <a class="nav-item nav-link" href="./yonetim_giris.php">YÖNETİM</a>
+      
+      
+    </div>
+  </div>
+</nav>
+  
+
+   </header>
+   <h1>KAYIT OL</h1>
 </head>
 <body>
+
+
+
 
 <!-- ------------------------------------------------------- -->
 <?php
@@ -56,6 +79,15 @@ if(isset($_GET['durum1'])=="hata")
         <button type="submit" name="insertislemi">KAYDET</button>
         
     </form>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
     
 </body>
 </html>
