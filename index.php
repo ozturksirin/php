bd4f3bad15d541fb53c8b5948a6b5d922fbde5cf
ozturk sirin <ozturk2002.zri@gmail.com>
2022-06-28 10:54:33 +0300

    fixed

--- index.php
+++ index.php
@@ -79,13 +79,6 @@ include("baglan.php");
     <a href="./cikis.php"> <button type="submit" class="btn btn-primary">Çıkış Yap</button></a>
 
 
-    
-
-
-    
-
-
-
     <!-- --------------------------------------------------------------------------------- -->
 
     <div class="image">
@@ -100,7 +93,7 @@ include("baglan.php");
         <img class="resim" src="kitap_resim/kitap8.jpg" width="400" height="600" /><br><br>
     </div> 
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
